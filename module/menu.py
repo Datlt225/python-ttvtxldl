da48fc0f9d6e82c56ba9c95770dfb93693277f72
NghiaPH <nghiaphamln3@gmail.com>
2020-11-11 22:07:56 +0700

    add CareerNeedJob feature

--- module/menu.py
+++ module/menu.py
@@ -133,12 +133,47 @@ def AllCareer():
         name.append(x)
         value.append(y)
 
-    # Chart carrer
+    # Chart career
     y_pos = np.arange(len(name))
     plt.bar(y_pos, value)
     plt.title("BIỂU ĐỒ SỐ LƯỢNG NGÀNH NGHỀ ĐANG TUYỂN DỤNG")
     plt.xticks(y_pos, name, rotation=90, fontsize=3)
-    plt.savefig('../image/chart.png', dpi=1500)
+    plt.savefig('../image/career-chart.png', dpi=1500)
 
 
-AllCareer()
+def CareerNeedJob():
+    counts = dict()
+    name = []
+    value = []
+
+    # Connect to database (Collection: job)
+    db = connectToDatabase('jobseeker')
+    data = db.find()
+
+    # Get all carrer
+    for x in data:
+        career = x['CareerAspirations']['expectedJobCategory']
+
+        for word in career.split(','):
+            word = ' '.join(word.split())
+            if word in counts:
+                counts[word] += 1
+            else:
+                counts[word] = 1
+
+    # Get name and value list
+    print("CHI TIẾT SỐ LƯỢNG NGÀNH NGHỀ ĐANG CẦN VIỆC LÀM: ")
+    for x, y in counts.items():
+        print(f'   - {x}: {y}')
+        name.append(x)
+        value.append(y)
+
+    # Chart career
+    y_pos = np.arange(len(name))
+    plt.bar(y_pos, value)
+    plt.title("BIỂU ĐỒ SỐ LƯỢNG NGÀNH NGHỀ ĐANG CẦN VIỆC LÀM")
+    plt.xticks(y_pos, name, rotation=90, fontsize=3)
+    plt.savefig('../image/chart-career-need-job.png', dpi=1500)
+
+
+CareerNeedJob()
